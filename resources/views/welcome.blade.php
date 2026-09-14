ecbdb3486356a274ad0ab2b90ede765c7cb2aebb
Ehsan Mirtalebi <ehsan.mirtalebi@gmail.com>
2026-09-14 16:57:59 +0330

    Change banner section to 2:1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
     <x-pet-type />
     <x-featured-products />
     <x-brands />
+    <x-featured-products />
     <x-promo-banner />
+    <x-featured-products />
     <x-why-us />
 </x-layouts.app>
